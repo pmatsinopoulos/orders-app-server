78932b22d6587412dc8a52c564ccfd642b47a62a
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-03-02 15:44:19 +0200

    Broadcast a cable message with the new order created

--- app/consumers/events_consumer.rb
+++ app/consumers/events_consumer.rb
@@ -2,7 +2,19 @@ class EventsConsumer < ApplicationConsumer
   private
 
   def consume
-    Order.create! params.payload['data'].merge(code: params.payload['event_id'])
+    order = Order.create! params.payload['data'].merge(code: params.payload['event_id'])
+    ActionCable.server.broadcast(
+      'application_messages',
+      {
+        type: 'NEW_ORDER_CREATED',
+        order: {
+          id: order.id,
+          code: order.code,
+          product: order.product,
+          price: order.price
+       }
+      }
+    )
   rescue => ex
     Rails.logger.error(ex)
   end
