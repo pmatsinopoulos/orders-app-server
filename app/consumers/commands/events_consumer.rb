075c9665017b8073ce29f4b256f8001389ac30a8
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-03-06 08:03:43 +0200

    Minor: call a method with the argument sent in parentheses

--- app/consumers/commands/events_consumer.rb
+++ app/consumers/commands/events_consumer.rb
@@ -3,8 +3,8 @@ module Commands
     private
 
     def consume
-      order = Order.create! params.payload['data'].merge(code: params.payload['event_id'])
-      send_response_with order
+      order = Order.create!(params.payload['data'].merge(code: params.payload['event_id']))
+      send_response_with(order)
     rescue => ex
       Rails.logger.error(ex)
     end
